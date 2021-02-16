2c17b52451177434d604257659ae418601c706ea
KemoNine <mcrosson@kemonine.info>
2021-02-16 17:31:39 -0500

    Enhance generator code to include full details and cheat sheet forms of markdown

--- artsey/artsey.py
+++ artsey/artsey.py
@@ -10,83 +10,242 @@
 #####
 # Imports
 #####
-import markdown
+import argparse
 import pprint
+import sys
 import yaml
 from copy import deepcopy
 
 #####
+# Globals
+#####
+key_active = '⚫'
+key_inactive = '⚪'
+combo_empty = [
+    [key_inactive, key_inactive, key_inactive, key_inactive], 
+    [key_inactive, key_inactive, key_inactive, key_inactive], 
+]
+
+#####
+# CLI Related
+#####
+parser = argparse.ArgumentParser(description='Parse ARTSEY.IO yaml into reference materials')
+parser.add_argument('--exclude-left', action='store_true',
+                    help='Exclude left handed information')
+parser.add_argument('--exclude-right', action='store_true',
+                    help='Exclude rigth handed information')
+parser.add_argument('--input-file', action='store', default='artsey.yaml',
+                    help='The ARTSEY.IO yaml definition to use for processing')
+parser.add_argument('--exclude-cheat-sheet-markdown', action='store_true',
+                    help='Exclude (skip) cheat sheet markdown processing')
+parser.add_argument('--exclude-detailed-markdown', action='store_true',
+                    help='Exclude (skip) detailed markdown processing')
+parser.add_argument('--cheat-sheet-markdown-file', action='store', default='artsey_cheat.md',
+                    help='The filename for writing the cheat sheat markdown')
+parser.add_argument('--detailed-markdown-file', action='store', default='artsey.md',
+                    help='The filename for writing the detailed markdown')
+parser.add_argument('--debug', action='store_true',
+                    help='Print debugging information')
+args = parser.parse_args()
+
+if args.exclude_left and args.exclude_right:
+    print('ERROR: You *MUST* not exclude both left and right')
+    sys.exit(1)
+
+if args.exclude_cheat_sheet_markdown and args.exclude_detailed_markdown:
+    print('ERROR: You *MUST* not exclude all output options')
+    sys.exit(1)
+
+#####
 # Load ARTSEY yaml definition
 #####
-stream = open('artsey.yaml', 'r')
-data = yaml.load(stream)
+stream = open(args.input_file, 'r')
+data = yaml.full_load(stream)
 
+#####
 # Debug print to ensure data is correct
-pprint.pprint(data)
+#####
+if args.debug:
+    pprint.pprint(data)
 
-with open('artsey.md', 'wb') as f:
-    f.write(b'# ARTSEY.IO Cheat Sheet\n')
-    f.write(b'\n')
-    f.write(b'## Version: ')
-    f.write(data['version'].encode())
-    f.write(b'\n')
-    f.write(b'\n')
-    f.write(b'## Changelog\n')
-    f.write(b'\n')
-    for change in data['changelog']:
-        f.write(b'- ')
-        f.write(change.encode())
-        f.write(b'\n')
-    f.write(b'\n')
-    f.write(b'## Layers\n')
-    f.write(b'\n')
-    f.write(b'| Index | Layer |\n')
-    f.write(b'| ----- | ----- |\n')
-    layers = []
-    for layer in data['layers']:
-        f.write(b'| ')
-        f.write(layer['layer'].encode())
-        f.write(b' | ')
-        f.write(str(layer['index']).encode())
-        f.write(b' |\n')
-    f.write(b'\n')
-
-    f.write(b'| Character | Layer | Combo |\n')
-    f.write(b'| --------- | ----- | ----- |\n')
-
-    key_active = '⚫'
-    key_inactive = '⚪'
-    combo_empty = [
-        [key_inactive, key_inactive, key_inactive, key_inactive], 
-        [key_inactive, key_inactive, key_inactive, key_inactive], 
-    ]
-
-    for code in data['keymap']:
-        left = deepcopy(combo_empty)
-        right = deepcopy(combo_empty)
-        f.write(b'| ')
-        f.write(str(code['description']).encode())
-        f.write(b' | ')
-        f.write(code['layer']['layer'].encode())
-        f.write(b' | ')
+#####
+# Convert combo to markdown
+#####
+def generate_combo_markdown(code):
+    to_return = b''
+    left = deepcopy(combo_empty)
+    right = deepcopy(combo_empty)
+    if not args.exclude_left:
         for key in code['combo']['left']:
             x = key['x']
             y = key['y']
             left[y][x] = key_active
+    if not args.exclude_right:
         for key in code['combo']['right']:
             x = key['x']
             y = key['y']
             right[y][x] = key_active
-        f.write(b'**Right**<br>')
+
+    to_return += b'| '
+    to_return += str(code['description']).replace('_', ' ').encode()
+    to_return += b' | '
+    to_return += code['layer']['layer'].replace('_', ' ').encode()
+    to_return += b' | '
+    if not args.exclude_right:
+        if not args.exclude_left:
+            to_return += b'**Right**<br>'
         for (y, row) in enumerate(right):
             for (x,column) in enumerate(row):
-                f.write(column.encode())
-            f.write(b'<br>')
-        f.write(b'<br>')
-        f.write(b'**Left**<br>')
+                to_return += column.encode()
+            to_return += b'<br>'
+    if not args.exclude_left and not args.exclude_right:
+        to_return += b'<br>'
+    if not args.exclude_left:
+        if not args.exclude_right:
+            to_return += b'**Left**<br>'
         for (y, row) in enumerate(left):
             for (x,column) in enumerate(row):
-                f.write(column.encode())
-            f.write(b'<br>')
-        f.write(b' |')
+                to_return += column.encode()
+            to_return += b'<br>'
+    to_return += b' |'
+    to_return += b'\n'
+
+
+    return to_return
+
+#####
+# Process cheat sheet markdown output and write to file
+#####
+if not args.exclude_cheat_sheet_markdown:
+    with open(args.cheat_sheet_markdown_file, 'wb') as f:
+        f.write(b'# ARTSEY.IO Cheat Sheet\n')
         f.write(b'\n')
+        if args.exclude_left:
+            f.write(b'# RIGHT HANDED LAYOUT\n')
+            f.write(b'\n')
+        if args.exclude_right:
+            f.write(b'# LEFT HANDED LAYOUT\n')
+            f.write(b'\n')
+        f.write(b'## Version: ')
+        f.write(data['version'].encode())
+        f.write(b'\n')
+        f.write(b'\n')
+        f.write(b'## Changelog\n')
+        f.write(b'\n')
+        for change in data['changelog']:
+            f.write(b'- ')
+            f.write(change.encode())
+            f.write(b'\n')
+        f.write(b'\n')
+        layer_data = {}
+        for layer in data['layers']:
+            layer_data[layer['layer']] = []
+        for code in data['keymap']:
+            layer_name = code['layer']['layer']
+            layer_data[layer_name].append(code)
+        for layer in layer_data.keys():
+            f.write(b'## Layer: ')
+            f.write(layer.replace('_', ' ').encode())
+            f.write(b'\n')
+            f.write(b'\n')
+            single_key_left = deepcopy(combo_empty)
+            single_key_right = deepcopy(combo_empty)
+            single_key_left_seen = False
+            single_key_right_seen = False
+            multi_key = []
+            for combo in layer_data[layer]:
+                if not args.exclude_right:
+                    if len(combo['combo']['right']) > 1:
+                        multi_key.append(generate_combo_markdown(combo))
+                    else:
+                        single_key_right_seen = True
+                        single_key_right[combo['combo']['right'][0]['y']][combo['combo']['right'][0]['x']] = combo['description']
+                if not args.exclude_left:
+                    if len(combo['combo']['left']) > 1:
+                        multi_key.append(generate_combo_markdown(combo))
+                    else:
+                        single_key_left_seen = True
+                        single_key_left[combo['combo']['left'][0]['y']][combo['combo']['left'][0]['x']] = combo['description']
+            
+            f.write(b'### Keys\n')
+            f.write(b'\n')
+            if not args.exclude_right:
+                if single_key_right_seen:
+                    if not args.exclude_left:
+                        f.write(b'#### Right\n')
+                        f.write(b'\n')
+                    f.write(b'| C1 | C2 | C3 | C4 |\n')
+                    f.write(b'| -- | -- | -- | -- |\n')
+                    for (y, row) in enumerate(single_key_right):
+                        for (x,column) in enumerate(row):
+                            f.write(b' ')
+                            f.write(str(column).replace('_', ' ').encode())
+                            f.write(b' |')
+                        f.write(b'\n')
+                    f.write(b'\n')
+            if not args.exclude_left:
+                if single_key_left_seen:
+                    if not args.exclude_right:
+                        f.write(b'#### Left\n')
+                        f.write(b'\n')
+                    f.write(b'| C1 | C2 | C3 | C4 |\n')
+                    f.write(b'| -- | -- | -- | -- |\n')
+                    for (y, row) in enumerate(single_key_left):
+                        for (x,column) in enumerate(row):
+                            f.write(b' ')
+                            f.write(str(column).replace('_', ' ').encode())
+                            f.write(b' |')
+                        f.write(b'\n')
+                    f.write(b'\n')
+
+            multi_key = set(multi_key)
+            if len(multi_key) > 0:
+                f.write(b'### Combos\n')
+                f.write(b'\n')
+                f.write(b'| Character | Layer | Combo |\n')
+                f.write(b'| --------- | ----- | ----- |\n')
+                for combo in multi_key:
+                    f.write(combo)
+
+
+#####
+# Process detailed markdown output and write to file
+#####
+if not args.exclude_detailed_markdown:
+    with open(args.detailed_markdown_file, 'wb') as f:
+        f.write(b'# ARTSEY.IO Detailed Layout\n')
+        f.write(b'\n')
+        if args.exclude_left:
+            f.write(b'# RIGHT HANDED LAYOUT\n')
+            f.write(b'\n')
+        if args.exclude_right:
+            f.write(b'# LEFT HANDED LAYOUT\n')
+            f.write(b'\n')
+        f.write(b'## Version: ')
+        f.write(data['version'].encode())
+        f.write(b'\n')
+        f.write(b'\n')
+        f.write(b'## Changelog\n')
+        f.write(b'\n')
+        for change in data['changelog']:
+            f.write(b'- ')
+            f.write(change.encode())
+            f.write(b'\n')
+        f.write(b'\n')
+        f.write(b'## Layers\n')
+        f.write(b'\n')
+        f.write(b'| Index | Layer |\n')
+        f.write(b'| ----- | ----- |\n')
+        layers = []
+        for layer in data['layers']:
+            f.write(b'| ')
+            f.write(layer['layer'].replace('_', ' ').encode())
+            f.write(b' | ')
+            f.write(str(layer['index']).encode())
+            f.write(b' |\n')
+        f.write(b'\n')
+
+        f.write(b'| Character | Layer | Combo |\n')
+        f.write(b'| --------- | ----- | ----- |\n')
+        for code in data['keymap']:
+            f.write(generate_combo_markdown(code))
---
+++ artsey/artsey_cheat.md
@@ -0,0 +1,241 @@
+# ARTSEY.IO Cheat Sheet
+
+## Version: v0.8
+
+## Changelog
+
+- Enter is now A+R+T+S
+- Z is now A+Y+I+O
+- Shift lock is now A+E
+- Add caps lock as A+R+E+Y
+- Add question mark to O on symbols layer
+- Moved page up/down to navigation layer
+- Add mouse wheel up/down to mouse layer
+- Add 'custom' layer so users can add additional items if desired (included custom layer is an *example*)
+- Custom layer key binding is S+O
+- Enhance F1-12 so they are on layers off the number layer and ensure all 12 are present
+- Make layers activate when held
+- Convert all mod keys (shift, ctrl, etc) to be leader keys
+- Add layer scopes to keys, including a 'global scope'
+- Layout ctrl/alt/gui as LEFT side keys to avoid possible confusion when right/left forms of these keys makes a difference for users
+
+## Layer: global
+
+### Keys
+
+### Combos
+
+| Character | Layer | Combo |
+| --------- | ----- | ----- |
+| backspace | global | **Right**<br>⚪⚫⚪⚪<br>⚫⚪⚪⚪<br><br>**Left**<br>⚪⚪⚫⚪<br>⚪⚪⚪⚫<br> |
+| control left | global | **Right**<br>⚪⚪⚪⚫<br>⚫⚪⚪⚪<br><br>**Left**<br>⚫⚪⚪⚪<br>⚪⚪⚪⚫<br> |
+| space | global | **Right**<br>⚪⚪⚪⚪<br>⚫⚫⚫⚫<br><br>**Left**<br>⚪⚪⚪⚪<br>⚫⚫⚫⚫<br> |
+| escape | global | **Right**<br>⚫⚫⚪⚪<br>⚪⚪⚪⚫<br><br>**Left**<br>⚪⚪⚫⚫<br>⚫⚪⚪⚪<br> |
+| shift lock | global | **Right**<br>⚫⚪⚪⚪<br>⚫⚪⚪⚪<br><br>**Left**<br>⚪⚪⚪⚫<br>⚪⚪⚪⚫<br> |
+| enter | global | **Right**<br>⚫⚫⚫⚫<br>⚪⚪⚪⚪<br><br>**Left**<br>⚫⚫⚫⚫<br>⚪⚪⚪⚪<br> |
+| alt left | global | **Right**<br>⚪⚪⚪⚫<br>⚪⚪⚫⚪<br><br>**Left**<br>⚫⚪⚪⚪<br>⚪⚫⚪⚪<br> |
+| gui left | global | **Right**<br>⚪⚪⚪⚫<br>⚪⚫⚪⚪<br><br>**Left**<br>⚫⚪⚪⚪<br>⚪⚪⚫⚪<br> |
+| shift left | global | **Right**<br>⚪⚫⚫⚫<br>⚫⚪⚪⚪<br><br>**Left**<br>⚫⚫⚫⚪<br>⚪⚪⚪⚫<br> |
+## Layer: base
+
+### Keys
+
+#### Right
+
+| C1 | C2 | C3 | C4 |
+| -- | -- | -- | -- |
+ a | r | t | s |
+ e | y | i | o |
+
+#### Left
+
+| C1 | C2 | C3 | C4 |
+| -- | -- | -- | -- |
+ s | t | r | a |
+ o | i | y | e |
+
+### Combos
+
+| Character | Layer | Combo |
+| --------- | ----- | ----- |
+| m | base | **Right**<br>⚪⚪⚪⚪<br>⚪⚫⚫⚫<br><br>**Left**<br>⚪⚪⚪⚪<br>⚫⚫⚫⚪<br> |
+| h | base | **Right**<br>⚪⚪⚪⚪<br>⚫⚪⚫⚪<br><br>**Left**<br>⚪⚪⚪⚪<br>⚪⚫⚪⚫<br> |
+| n | base | **Right**<br>⚪⚪⚪⚪<br>⚪⚪⚫⚫<br><br>**Left**<br>⚪⚪⚪⚪<br>⚫⚫⚪⚪<br> |
+| u | base | **Right**<br>⚪⚪⚪⚪<br>⚪⚫⚫⚪<br><br>**Left**<br>⚪⚪⚪⚪<br>⚪⚫⚫⚪<br> |
+| q | base | **Right**<br>⚫⚪⚫⚫<br>⚪⚪⚪⚪<br><br>**Left**<br>⚫⚫⚪⚫<br>⚪⚪⚪⚪<br> |
+| v | base | **Right**<br>⚪⚫⚪⚫<br>⚪⚪⚪⚪<br><br>**Left**<br>⚫⚪⚫⚪<br>⚪⚪⚪⚪<br> |
+| k | base | **Right**<br>⚪⚪⚪⚪<br>⚪⚫⚪⚫<br><br>**Left**<br>⚪⚪⚪⚪<br>⚫⚪⚫⚪<br> |
+| p | base | **Right**<br>⚪⚪⚪⚪<br>⚫⚪⚫⚫<br><br>**Left**<br>⚪⚪⚪⚪<br>⚫⚫⚪⚫<br> |
+| g | base | **Right**<br>⚪⚫⚫⚪<br>⚪⚪⚪⚪<br><br>**Left**<br>⚪⚫⚫⚪<br>⚪⚪⚪⚪<br> |
+| d | base | **Right**<br>⚫⚫⚫⚪<br>⚪⚪⚪⚪<br><br>**Left**<br>⚪⚫⚫⚫<br>⚪⚪⚪⚪<br> |
+| ' | base | **Right**<br>⚫⚪⚪⚪<br>⚪⚫⚫⚪<br><br>**Left**<br>⚪⚪⚪⚫<br>⚪⚫⚫⚪<br> |
+| l | base | **Right**<br>⚪⚪⚪⚪<br>⚫⚫⚫⚪<br><br>**Left**<br>⚪⚪⚪⚪<br>⚪⚫⚫⚫<br> |
+| w | base | **Right**<br>⚫⚪⚪⚫<br>⚪⚪⚪⚪<br><br>**Left**<br>⚫⚪⚪⚫<br>⚪⚪⚪⚪<br> |
+| b | base | **Right**<br>⚪⚪⚪⚪<br>⚫⚪⚪⚫<br><br>**Left**<br>⚪⚪⚪⚪<br>⚫⚪⚪⚫<br> |
+| c | base | **Right**<br>⚪⚪⚪⚪<br>⚫⚫⚪⚪<br><br>**Left**<br>⚪⚪⚪⚪<br>⚪⚪⚫⚫<br> |
+| . | base | **Right**<br>⚫⚪⚪⚪<br>⚪⚫⚪⚪<br><br>**Left**<br>⚪⚪⚪⚫<br>⚪⚪⚫⚪<br> |
+| / | base | **Right**<br>⚫⚪⚪⚪<br>⚪⚪⚪⚫<br><br>**Left**<br>⚪⚪⚪⚫<br>⚫⚪⚪⚪<br> |
+| caps lock | base | **Right**<br>⚫⚫⚪⚪<br>⚫⚫⚪⚪<br><br>**Left**<br>⚪⚪⚫⚫<br>⚪⚪⚫⚫<br> |
+| z | base | **Right**<br>⚫⚪⚪⚪<br>⚪⚫⚫⚫<br><br>**Left**<br>⚪⚪⚪⚫<br>⚫⚫⚫⚪<br> |
+| , | base | **Right**<br>⚫⚪⚪⚪<br>⚪⚪⚫⚪<br><br>**Left**<br>⚪⚪⚪⚫<br>⚪⚫⚪⚪<br> |
+| x | base | **Right**<br>⚪⚫⚫⚫<br>⚪⚪⚪⚪<br><br>**Left**<br>⚫⚫⚫⚪<br>⚪⚪⚪⚪<br> |
+| layer mouse | base | **Right**<br>⚫⚪⚫⚪<br>⚪⚫⚪⚪<br><br>**Left**<br>⚪⚫⚪⚫<br>⚪⚪⚫⚪<br> |
+| f | base | **Right**<br>⚫⚫⚪⚪<br>⚪⚪⚪⚪<br><br>**Left**<br>⚪⚪⚫⚫<br>⚪⚪⚪⚪<br> |
+| j | base | **Right**<br>⚪⚪⚫⚫<br>⚪⚪⚪⚪<br><br>**Left**<br>⚫⚫⚪⚪<br>⚪⚪⚪⚪<br> |
+## Layer: numbers
+
+### Keys
+
+#### Right
+
+| C1 | C2 | C3 | C4 |
+| -- | -- | -- | -- |
+ 1 | 2 | 3 | layer f one six |
+ 4 | 5 | 6 | layer f seven twelve |
+
+#### Left
+
+| C1 | C2 | C3 | C4 |
+| -- | -- | -- | -- |
+ layer f one six | 3 | 2 | 1 |
+ layer f seven twelve | 6 | 5 | 4 |
+
+### Combos
+
+| Character | Layer | Combo |
+| --------- | ----- | ----- |
+| 7 | numbers | **Right**<br>⚫⚫⚪⚪<br>⚪⚪⚪⚪<br><br>**Left**<br>⚪⚪⚫⚫<br>⚪⚪⚪⚪<br> |
+| 0 | numbers | **Right**<br>⚪⚪⚪⚪<br>⚪⚫⚫⚪<br><br>**Left**<br>⚪⚪⚪⚪<br>⚪⚫⚫⚪<br> |
+| 8 | numbers | **Right**<br>⚪⚫⚫⚪<br>⚪⚪⚪⚪<br><br>**Left**<br>⚪⚫⚫⚪<br>⚪⚪⚪⚪<br> |
+| 9 | numbers | **Right**<br>⚪⚪⚪⚪<br>⚫⚫⚪⚪<br><br>**Left**<br>⚪⚪⚪⚪<br>⚪⚪⚫⚫<br> |
+## Layer: symbols
+
+### Keys
+
+#### Right
+
+| C1 | C2 | C3 | C4 |
+| -- | -- | -- | -- |
+ ⚪ | " | ; | ! |
+ ⚪ | - | = | ? |
+
+#### Left
+
+| C1 | C2 | C3 | C4 |
+| -- | -- | -- | -- |
+ ! | ; | " | ⚪ |
+ ? | = | - | ⚪ |
+
+## Layer: parentheticals
+
+### Keys
+
+#### Right
+
+| C1 | C2 | C3 | C4 |
+| -- | -- | -- | -- |
+ ⚪ | ( | ) | { |
+ ⚪ | [ | ] | } |
+
+#### Left
+
+| C1 | C2 | C3 | C4 |
+| -- | -- | -- | -- |
+ { | ( | ) | ⚪ |
+ } | [ | ] | ⚪ |
+
+## Layer: navigation
+
+### Keys
+
+#### Right
+
+| C1 | C2 | C3 | C4 |
+| -- | -- | -- | -- |
+ home | arrow up | end | page up |
+ arrow left | ⚪ | arrow right | page down |
+
+#### Left
+
+| C1 | C2 | C3 | C4 |
+| -- | -- | -- | -- |
+ page up | home | arrow up | end |
+ page down | arrow left | ⚪ | arrow right |
+
+## Layer: f one six
+
+### Keys
+
+#### Right
+
+| C1 | C2 | C3 | C4 |
+| -- | -- | -- | -- |
+ F1 | F2 | F3 | ⚪ |
+ F4 | F5 | F6 | ⚪ |
+
+#### Left
+
+| C1 | C2 | C3 | C4 |
+| -- | -- | -- | -- |
+ ⚪ | F3 | F2 | F1 |
+ ⚪ | F6 | F5 | F4 |
+
+## Layer: f seven twelve
+
+### Keys
+
+#### Right
+
+| C1 | C2 | C3 | C4 |
+| -- | -- | -- | -- |
+ F7 | F8 | F9 | ⚪ |
+ F10 | F11 | F12 | ⚪ |
+
+#### Left
+
+| C1 | C2 | C3 | C4 |
+| -- | -- | -- | -- |
+ ⚪ | F9 | F8 | F7 |
+ ⚪ | F12 | F11 | F10 |
+
+## Layer: mouse
+
+### Keys
+
+#### Right
+
+| C1 | C2 | C3 | C4 |
+| -- | -- | -- | -- |
+ mouse button 1 | mouse move up | mouse button 2 | mouse wheel up |
+ mouse move left | mouse move down | mouse move right | mouse wheel down |
+
+#### Left
+
+| C1 | C2 | C3 | C4 |
+| -- | -- | -- | -- |
+ mouse wheel up | mouse button 2 | mouse move up | mouse button 1 |
+ mouse wheel down | mouse move left | mouse move down | mouse move right |
+
+## Layer: custom
+
+### Keys
+
+#### Right
+
+| C1 | C2 | C3 | C4 |
+| -- | -- | -- | -- |
+ Mute | Insert | Right Control | Volume Up |
+ Right Shift | Print Screen | Right Alt | Volumn Down |
+
+#### Left
+
+| C1 | C2 | C3 | C4 |
+| -- | -- | -- | -- |
+ Volume Up | Right Control | Insert | Mute |
+ Volumn Down | Right Alt | Print Screen | Right Shift |
+
+### Combos
+
+| Character | Layer | Combo |
+| --------- | ----- | ----- |
+| layer custom | custom | **Right**<br>⚪⚪⚪⚫<br>⚪⚪⚪⚫<br><br>**Left**<br>⚫⚪⚪⚪<br>⚫⚪⚪⚪<br> |
